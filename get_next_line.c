48f29b9c1735660a087284588cb1a9b7fecf437f
bbourcy <djnono23@me.com>
2022-07-15 14:43:09 +0200

    edit inter union & add shorter gnl

--- get_next_line.c
+++ get_next_line.c
@@ -7,6 +7,88 @@
 # define BUFFER_SIZE 1
 #endif
 
+char	*get_next_line(int fd)
+{
+	char	a[999999];
+	char	buffer[1];
+	char	*new_a;
+	int		i;
+ 
+	if (fd < 0 && BUFFER_SIZE <= 0)
+		return (NULL);
+	i = 0;
+	a[i] = 0;
+	while (read(fd, buffer, 1) == 1)
+	{
+		a[i] = buffer[0];
+		a[i + 1] = '\0';
+		if (a[i] == '\n')
+			break;
+		i++;
+	}
+	if (!a[0])
+		return (NULL);
+	new_a = malloc(i + 1);
+	if (!new_a)
+		return (NULL);
+	i = 0;
+	while (a[i])
+	{
+		new_a[i] = a[i];
+		i++;
+	}
+	new_a[i] = '\0';
+	return (new_a);
+}
+
+int main(int ac, char **av)
+{
+	int		fd;
+	char	*line;
+	(void)	ac;
+
+	fd = open(av[1], O_RDONLY);
+    while ((line = get_next_line(fd)) != 0)
+	{
+		printf("%s", line);
+		free(line);
+	}
+	close(fd);
+//	system("leaks a.out");
+	return (0);
+}
+
+// int	get_next_line(char **line)
+// {
+// 	int		i;
+// 	int		l;
+// 	int		r;
+// 	char	c;
+// 	char	*tmp;
+
+// 	r = 0;
+// 	l = 1;
+// 	if (!(*line = malloc(l)))
+// 		return (-1);
+// 	(*line)[0] = 0;
+// 	while ((r = read(0, &c, 1)) && l++ && c != '\n')
+// 	{
+// 		if (!(tmp = malloc(l)))
+// 		{
+// 			free(*line);
+// 			return (-1);
+// 		}
+// 		i = -1;
+// 		while (++i < l - 2)
+// 			tmp[i] = (*line)[i];
+// 		tmp[i] = c;
+// 		tmp[i + 1] = 0;
+// 		free(*line);
+// 		*line = tmp;
+// 	}
+// 	return (r);
+// }
+
 // char	*ft_strjoin(char *s, char c)
 // {
 // 	char	*str;
@@ -61,54 +143,3 @@
 // 	}
 // 	return (line);
 // }
-
-char	*get_next_line(int fd)
-{
-	char	a[999999];
-	char	buffer[1];
-	char	*new_a;
-	int		i;
- 
-	if (fd < 0 && BUFFER_SIZE <= 0)
-		return (NULL);
-	i = 0;
-	a[i] = 0;
-	while (read(fd, buffer, 1) == 1)
-	{
-		a[i] = buffer[0];
-		a[i + 1] = '\0';
-		if (a[i] == '\n')
-			break;
-		i++;
-	}
-	if (!a[0])
-		return (NULL);
-	new_a = malloc(i + 1);
-	if (!new_a)
-		return (NULL);
-	i = 0;
-	while (a[i])
-	{
-		new_a[i] = a[i];
-		i++;
-	}
-	new_a[i] = '\0';
-	return (new_a);
-}
-
-int main(int ac, char **av)
-{
-	int		fd;
-	char	*line;
-	(void)	ac;
-
-	fd = open(av[1], O_RDONLY);
-    while ((line = get_next_line(fd)) != 0)
-	{
-		printf("%s", line);
-		free(line);
-	}
-	close(fd);
-//	system("leaks a.out");
-	return (0);
-}
--- inter.c
+++ inter.c
@@ -1,5 +1,4 @@
 #include <unistd.h>
-#include <stdio.h>
 
 int	main(int argc, char **argv)
 {
@@ -33,40 +32,3 @@ int	main(int argc, char **argv)
 	write(1, "\n", 1);
 	return (0);
 }
-
-// void	inter(const char *s1, const char *s2)
-// {
-// 	char	checkalpha[127];
-// 	int		i;
-// 	int		j;
-	
-// 	if (s1 && s2)
-// 	{
-// 		i = 0;
-// 		while (i < 127)
-// 		{
-// 			checkalpha[i] = 0;
-// 		   i++;
-// 		}
-// 		i = 0;
-// 		while (s2[i] != 0)
-// 		{
-// 			j =	s2[i];
-// 			if (checkalpha[j] == 0)
-// 				checkalpha[j] = 1;
-// 			i++;
-// 		}
-// 		i = 0;
-// 		while (s1[i] != 0)
-// 		{
-// 			j = s1[i];
-// 			if (checkalpha[j] == 1)
-// 			{
-// 				write(1, &s1[i], 1);
-// 				checkalpha[j]= 2;
-// 			}
-// 			i++;
-// 		}
-// 	}
-// 	write(1, "\n", 1);
-// }
